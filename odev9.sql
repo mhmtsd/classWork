a7b6286385c52e8b22ce868eeaeaf1b619018fe1
mhmtsd <58916612+mhmtsd@users.noreply.github.com>
2021-08-28 14:15:32 +0300

    Update odev9.sql
    patika.dev sql odev 9

--- odev9.sql
+++ odev9.sql
@@ -10,3 +10,10 @@ inner join city ci on ci.country_id=country.country_id
 
 select first_name,last_name from customer 
 inner join payment p on customer.customer_id=p.customer_id
+
+
+--customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name 
+--isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız
+
+select r.rental_id, c.first_name,c.last_name from customer c
+inner join rental r on c.customer_id=r.customer_id
